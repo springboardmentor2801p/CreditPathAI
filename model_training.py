ead59deb4cf24021e162b884a3bac293b7c1a94a
Akhil1238 <148357292+Akhil1238@users.noreply.github.com>
2026-02-16 00:03:56 +0530

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv("data/processed/preprocessed.csv")
+# Load cleaned dataset
+df = pd.read_csv("data/processed/cleaned.csv")
 
 print(df.shape)
+
+# Duplicate check
+print("Duplicate rows:", df.duplicated().sum())
