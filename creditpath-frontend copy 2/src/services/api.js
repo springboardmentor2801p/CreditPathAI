a6696eb0de1c5653a8bbd51b03d833c213120db8
Yamini-srgari <syaminireddy2006@gmail.com>
2026-04-02 16:03:39 +0530

    Delete creditpath-frontend copy 2 directory

--- creditpath-frontend copy 2/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const API_URL = 'http://127.0.0.1:8000'
-
-export const predictRisk = async (borrowerData) => {
-  const response = await axios.post(`${API_URL}/predict`, borrowerData)
-  return response.data
-}
